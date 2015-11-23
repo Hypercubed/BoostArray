60f12c0807f084b504b7009c8ce13af10e06e70e
J. Harshbarger <hypercubed@gmail.com>
2015-11-23 21:57:12 +0900

    semistandard tests

--- tests/BoostArray-prototype-test.js
+++ tests/BoostArray-prototype-test.js
@@ -1,96 +1,91 @@
+/* global describe, it, beforeEach */
 
-var BoostArray = require('../BoostArray.js'),
-		chai = require('chai'),
-		expect = chai.expect,
-		sinon = require('sinon');
+var BoostArray = require('../BoostArray.js');
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
 
-describe('BoostArray(Array.prototype)', function() {
-	BoostArray(Array.prototype);
+describe('BoostArray(Array.prototype)', function () {
+  BoostArray(Array.prototype);
 
-	describe('constructor', function() {
+  describe('constructor', function () {
+    it('should add a $boosted flag', function () {
+      var myArray = [];
+      expect(myArray.$boosted).to.equal(true);
+    });
 
-		it('should add a $boosted flag', function() {
-			var myArray = [];
-			expect(myArray.$boosted).to.equal(true);
-		});
+    it('should add a isBoostedArray method', function () {
+      expect(BoostArray.isBoostedArray([1, 2, 3])).to.be.true();
+      expect(BoostArray.isBoostedArray(BoostArray())).to.be.true();
+    });
+  });
 
-		it('should add a isBoostedArray method', function() {
-			expect(BoostArray.isBoostedArray([1,2,3])).to.be.true();
-			expect(BoostArray.isBoostedArray(BoostArray())).to.be.true();
-		});
+  describe('#forEach', function () {
+    var plainArray, spy;
 
-	});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6];
+      spy = sinon.spy();
+    });
 
-	describe('#forEach', function() {
-		var plainArray, spy;
+    it('should run on plain array', function () {
+      plainArray.$forEach(spy);
+      expect(spy.callCount).to.equal(6);
+    });
+  });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6];
-			spy = sinon.spy();
-		});
+  describe('#map', function () {
+    var plainArray, spy;
 
-		it('should run on plain array', function() {
-			plainArray.$forEach(spy);
-			expect(spy.callCount).to.equal(6);
-		});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6, 15];
+      spy = sinon.spy(function (d) {
+        var s = [];
+        if (d % 3 === 0) { s.push('Fizz'); }
+        if (d % 5 === 0) { s.push('Buzz'); }
 
-	});
+        return s.length > 0 ? s.join(' ') : d;
+      });
+    });
 
-	describe('#map', function() {
-		var plainArray, spy;
+    it('should run on boosted array', function () {
+      var result = plainArray.$map(spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 'Fizz Buzz']);
+    });
+  });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6,15];
-			spy = sinon.spy(function(d) {
-				var s = [];
-				if (d % 3 === 0) { s.push('Fizz'); }
-				if (d % 5 === 0) { s.push('Buzz'); }
+  describe('#reduce', function () {
+    var plainArray, spy;
 
-				return s.length > 0 ? s.join(' ') : d;
-			});
-		});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6];
+      spy = sinon.spy(function (p, d) {
+        return p + d;
+      });
+    });
 
-		it('should run on boosted array', function() {
-			var result = plainArray.$map(spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([1,2,'Fizz',4,'Buzz','Fizz','Fizz Buzz']);
-		});
+    it('should run on boosted array', function () {
+      var result = plainArray.$reduce(spy, 0);
+      expect(spy.callCount).to.equal(6);
+      expect(result).to.equal(21);
+    });
+  });
 
-	});
+  describe('#filter', function () {
+    var plainArray, spy;
 
-	describe('#reduce', function() {
-		var plainArray, spy;
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6, 15];
+      spy = sinon.spy(function (d) {
+        return d % 3 === 0 || d % 5 === 0;
+      });
+    });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6];
-			spy = sinon.spy(function(p, d) {
-				return p+d;
-			});
-		});
-
-		it('should run on boosted array', function() {
-			var result = plainArray.$reduce(spy, 0);
-			expect(spy.callCount).to.equal(6);
-			expect(result).to.equal(21);
-		});
-
-	});
-
-	describe('#filter', function() {
-		var plainArray, spy;
-
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6,15];
-			spy = sinon.spy(function(d) {
-				return d % 3 === 0 || d % 5 === 0;
-			});
-		});
-
-		it('should run on boosted array', function() {
-			var result = plainArray.$filter(spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([3,5,6,15]);
-		});
-
-	});
+    it('should run on boosted array', function () {
+      var result = plainArray.$filter(spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([3, 5, 6, 15]);
+    });
+  });
 });
--- tests/BoostArray-spec.js
+++ tests/BoostArray-spec.js
@@ -1,187 +1,176 @@
+/* global describe, it, beforeEach */
 
-var BoostArray = require('../BoostArray.js'),
-		chai = require('chai'),
-		expect = chai.expect,
-		sinon = require('sinon');
+var BoostArray = require('../BoostArray.js');
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
 
-console.log(BoostArray);
+describe('BoostArray', function () {
+  describe('constructor', function () {
+    it('should be a constructor-like method', function () {
+      expect(BoostArray).to.be.a('function');
+    });
 
-describe('BoostArray', function() {
+    it('should add a isArray method', function () {
+      expect(BoostArray).to.have.ownProperty('isArray');
+      expect(BoostArray.isArray([1, 2, 3])).to.be.true();
+    });
 
-	describe('constructor', function() {
+    it('should add a isBoostedArray method', function () {
+      expect(BoostArray).to.have.ownProperty('isBoostedArray');
+      expect(BoostArray.isBoostedArray([1, 2, 3])).to.be.false();
+      expect(BoostArray.isBoostedArray(BoostArray())).to.be.true();
+    });
 
-		it('should be a constructor-like method', function() {
-			expect(BoostArray).to.be.a('function');
-		});
+    it('should create an new array', function () {
+      var myArray = BoostArray();
+      expect(myArray).to.have.length(0);
+      expect(myArray).to.be.instanceof(Array);
+      expect(Array.isArray(myArray)).to.equal(true);
+    });
 
-		it('should add a isArray method', function() {
-			expect(BoostArray).to.have.ownProperty('isArray');
-			expect(BoostArray.isArray([1,2,3])).to.be.true();
-		});
+    it('should attach to an existing array', function () {
+      var myArray = [1, 2, 3];
+      BoostArray(myArray);
+      expect(myArray).to.have.length(3);
+      expect(myArray).to.be.instanceof(Array);
+      expect(Array.isArray(myArray)).to.equal(true);
+    });
 
-		it('should add a isBoostedArray method', function() {
-			expect(BoostArray).to.have.ownProperty('isBoostedArray');
-			expect(BoostArray.isBoostedArray([1,2,3])).to.be.false();
-			expect(BoostArray.isBoostedArray(BoostArray())).to.be.true();
-		});
+    it('should add a $boosted flag', function () {
+      var myArray = BoostArray();
+      expect(myArray.$boosted).to.equal(true);
+    });
+  });
 
-		it('should create an new array', function() {
-			var myArray = BoostArray();
-			expect(myArray).to.have.length(0);
-			expect(myArray).to.be.instanceof(Array);
-			expect(Array.isArray(myArray)).to.equal(true);
-		});
+  describe('#forEach', function () {
+    var plainArray, boostedArray, spy;
 
-		it('should attach to an existing array', function() {
-			var myArray = [1,2,3];
-			BoostArray(myArray);
-			expect(myArray).to.have.length(3);
-			expect(myArray).to.be.instanceof(Array);
-			expect(Array.isArray(myArray)).to.equal(true);
-		});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6];
+      boostedArray = BoostArray(plainArray.slice(0));
+      spy = sinon.spy();
+    });
 
-		it('should add a $boosted flag', function() {
-			var myArray = BoostArray();
-			expect(myArray.$boosted).to.equal(true);
-		});
+    it('should be non-enumerable', function () {
+      var myArray = BoostArray([1, 2, 3]);
+      expect(myArray).to.have.ownProperty('$forEach');
+      expect(myArray.propertyIsEnumerable('$forEach')).to.be.false();
+    });
 
-	});
+    it('should run on boosted array', function () {
+      boostedArray.$forEach(spy);
+      expect(spy.callCount).to.equal(6);
+    });
 
-	describe('#forEach', function() {
-		var plainArray, boostedArray, spy;
+    it('can be invoked on plain array', function () {
+      BoostArray.prototype.$forEach.call(plainArray, spy);
+      expect(spy.callCount).to.equal(6);
+    });
+  });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6];
-			boostedArray = BoostArray(plainArray.slice(0));
-			spy = sinon.spy();
-		});
+  describe('#map', function () {
+    var plainArray, boostedArray, spy;
 
-		it('should be non-enumerable', function() {
-			var myArray = BoostArray([1,2,3]);
-			expect(myArray).to.have.ownProperty('$forEach');
-			expect(myArray.propertyIsEnumerable('$forEach')).to.be.false();
-		});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6, 15];
+      boostedArray = BoostArray(plainArray.slice(0));
+      spy = sinon.spy(function (d) {
+        var s = [];
+        if (d % 3 === 0) { s.push('Fizz'); }
+        if (d % 5 === 0) { s.push('Buzz'); }
 
-		it('should run on boosted array', function() {
-			boostedArray.$forEach(spy);
-			expect(spy.callCount).to.equal(6);
-		});
+        return s.length > 0 ? s.join(' ') : d;
+      });
+    });
 
-		it('can be invoked on plain array', function() {
-			BoostArray.prototype.$forEach.call(plainArray, spy);
-			expect(spy.callCount).to.equal(6);
-		});
+    it('should run on boosted array', function () {
+      var result = boostedArray.$map(spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 'Fizz Buzz']);
+    });
 
-	});
+    it('can be invoked on plain array', function () {
+      var result = BoostArray.prototype.$map.call(plainArray, spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 'Fizz Buzz']);
+    });
+  });
 
-	describe('#map', function() {
-		var plainArray, boostedArray, spy;
+  describe('#reduce', function () {
+    var plainArray, boostedArray, spy;
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6,15];
-			boostedArray = BoostArray(plainArray.slice(0));
-			spy = sinon.spy(function(d) {
-				var s = [];
-				if (d % 3 === 0) { s.push('Fizz'); }
-				if (d % 5 === 0) { s.push('Buzz'); }
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6];
+      boostedArray = BoostArray(plainArray.slice(0));
+      spy = sinon.spy(function (p, d) {
+        return p + d;
+      });
+    });
 
-				return s.length > 0 ? s.join(' ') : d;
-			});
-		});
+    it('should run on boosted array', function () {
+      var result = boostedArray.$reduce(spy, 0);
+      expect(spy.callCount).to.equal(6);
+      expect(result).to.equal(21);
+    });
 
-		it('should run on boosted array', function() {
-			var result = boostedArray.$map(spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([1,2,'Fizz',4,'Buzz','Fizz','Fizz Buzz']);
-		});
+    it('can be invoked on plain array', function () {
+      var result = BoostArray.prototype.$reduce.call(plainArray, spy, 0);
+      expect(spy.callCount).to.equal(6);
+      expect(result).to.equal(21);
+    });
+  });
 
-		it('can be invoked on plain array', function() {
-			var result = BoostArray.prototype.$map.call(plainArray, spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([1,2,'Fizz',4,'Buzz','Fizz','Fizz Buzz']);
-		});
+  describe('#filter', function () {
+    var plainArray, boostedArray, spy;
 
-	});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 5, 6, 15];
+      boostedArray = BoostArray(plainArray);
+      spy = sinon.spy(function (d) {
+        return d % 3 === 0 || d % 5 === 0;
+      });
+    });
 
-	describe('#reduce', function() {
-		var plainArray, boostedArray, spy;
+    it('should run on boosted array', function () {
+      var result = boostedArray.$filter(spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([3, 5, 6, 15]);
+    });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6];
-			boostedArray = BoostArray(plainArray.slice(0));
-			spy = sinon.spy(function(p, d) {
-				return p+d;
-			});
-		});
+    it('can be invoked on plain array', function () {
+      var result = BoostArray.prototype.$filter.call(plainArray, spy);
+      expect(spy.callCount).to.equal(7);
+      expect(result).to.deep.equal([3, 5, 6, 15]);
+    });
+  });
 
-		it('should run on boosted array', function() {
-			var result = boostedArray.$reduce(spy, 0);
-			expect(spy.callCount).to.equal(6);
-			expect(result).to.equal(21);
-		});
+  describe('#indexOf', function () {
+    var plainArray, boostedArray;
 
-		it('can be invoked on plain array', function() {
-			var result = BoostArray.prototype.$reduce.call(plainArray, spy, 0);
-			expect(spy.callCount).to.equal(6);
-			expect(result).to.equal(21);
-		});
+    beforeEach(function () {
+      plainArray = [1, 2, 3, 4, 'buzz', 6];
+      boostedArray = BoostArray(plainArray.slice(0));
+    });
 
-	});
+    it('should run on boosted array', function () {
+      var result = boostedArray.$indexOf(3);
+      expect(result).to.equal(2);
+    });
 
-	describe('#filter', function() {
-		var plainArray, boostedArray, spy;
+    it('can be invoked on plain array', function () {
+      var result = BoostArray.prototype.$indexOf.call(plainArray, 3);
+      expect(result).to.equal(2);
+    });
 
-		beforeEach(function() {
-			plainArray = [1,2,3,4,5,6,15];
-			boostedArray = BoostArray(plainArray);
-			spy = sinon.spy(function(d) {
-				return d % 3 === 0 || d % 5 === 0;
-			});
-		});
+    it('should return -1 if not found', function () {
+      var result = boostedArray.$indexOf('fizz');
+      expect(result).to.equal(-1);
+    });
 
-		it('should run on boosted array', function() {
-			var result = boostedArray.$filter(spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([3,5,6,15]);
-		});
-
-		it('can be invoked on plain array', function() {
-			var result = BoostArray.prototype.$filter.call(plainArray, spy);
-			expect(spy.callCount).to.equal(7);
-			expect(result).to.deep.equal([3,5,6,15]);
-		});
-
-	});
-
-	describe('#indexOf', function() {
-		var plainArray, boostedArray, spy;
-
-		beforeEach(function() {
-			plainArray = [1,2,3,4,'buzz',6];
-			boostedArray = BoostArray(plainArray.slice(0));
-		});
-
-		it('should run on boosted array', function() {
-			var result = boostedArray.$indexOf(3);
-			expect(result).to.equal(2);
-		});
-
-		it('can be invoked on plain array', function() {
-			var result = BoostArray.prototype.$indexOf.call(plainArray, 3);
-			expect(result).to.equal(2);
-		});
-
-		it('should return -1 if not found', function() {
-			var result = boostedArray.$indexOf('fizz');
-			expect(result).to.equal(-1);
-		});
-
-		it('should return -1 if array is empty', function() {
-			var result = BoostArray([]).$indexOf('buzz');
-			expect(result).to.equal(-1);
-		});
-
-
-	});
-
+    it('should return -1 if array is empty', function () {
+      var result = BoostArray([]).$indexOf('buzz');
+      expect(result).to.equal(-1);
+    });
+  });
 });
